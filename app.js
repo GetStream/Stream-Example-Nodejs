0d99bf452affc634f59eca3e27300e833f1dc3eb
mihaineacsu <mihai.neacsu@ymail.com>
2015-02-18 22:44:44 +0200

    Set up connection to mongodb through mongoose

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var bodyParser = require('body-parser'),
 	config = require('./config'),
 	express = require('express'),
-	expressLayouts = require('express-ejs-layouts');
+	expressLayouts = require('express-ejs-layouts'),
+	mongoose = require('mongoose');
 
 var app = express();
 
@@ -11,5 +12,10 @@ app.set('layout', 'myLayout');
 app.use(expressLayouts);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+var db_connection = mongoose.createConnection(config.db);
+db_connection.on('error', function () {
+  console.log('Error! Database connection failed.');
+});
 
 app.listen(config.port);
