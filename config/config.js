78c9edb9c38cb08aebd8294496256b686384d277
mihaineacsu <mihai.neacsu@ymail.com>
2015-03-06 16:56:52 +0200

    Use different app credentials for local execution

--- config/config.js
+++ config/config.js
@@ -7,9 +7,9 @@ nconf.defaults({
 	'GITHUB_CLIENT_ID': 'd267d4096edf2f3b0180',
 	'GITHUB_CLIENT_SECRET': '5d9830f534cc7477bd857f616845977f16f28605',
 	'GITHUB_CALLBACK': 'http://127.0.0.1:8000/auth/github/callback',
-	'STREAM_ID': '2350',
-	'STREAM_API_KEY': 'w2dnzgtjezph',
-	'STREAM_API_SECRET': '7zy6jrpmzdfrnv9yhjaw9ss6etsrn62hdet4vbyyprkgdqpddhjuugrywqh74war'
+	'STREAM_ID': '2637',
+	'STREAM_API_KEY': '2kfz86z8qdd9',
+	'STREAM_API_SECRET': 'xn8g8und66vza9q43sh9kpbfnrkexhy3kf7vns7wgkhsryt3d85w5f8w8r397gc5'
 });
 
 module.exports = nconf;
