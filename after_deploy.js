3304fb29ad3ea71c7776e91347d7114477d87490
mihaineacsu <mihai.neacsu@ymail.com>
2015-03-09 00:51:04 +0200

    Make after_deploy.js sequential execution, prevent issues with fixtures

--- after_deploy.js
+++ after_deploy.js
@@ -1,5 +1,6 @@
 var models = require('./models'),
-    fs = require('fs');
+    fs = require('fs').
+    async = require('async');
 
 var User = models.User,
     Item = models.Item,
@@ -7,36 +8,44 @@ var User = models.User,
     Follow = models.Follow;
 
 module.exports = function(){
-    User.create({username: 'admin', avatar_url: 'admin.jpg'}, function(){
-        User.create({username: 'Andrew', avatar_url: 'https://github.com/identicons/jasonlong.png'});
-        User.create({username: 'Sergey', avatar_url: 'https://avatars0.githubusercontent.com/u/4436860?v=3&s=96'});
-        User.create({username: 'Thomas', avatar_url: 'https://avatars0.githubusercontent.com/u/125464?v=3&s=96'});
-    });
-
-    var mediaFiles = fs.readdirSync('./media/');
-    mediaFiles.forEach(function(fileName){
-        Item.create({user: 0, image_url: fileName},
-            function(err, newArticle){
-                if (err){
-                    return console.log(err);
-                }
-
-        });
-    });
-
-    Pin.as_activity({user: 1, item: 1});
-    Pin.as_activity({user: 1, item: 2});
-    Pin.as_activity({user: 1, item: 3});
-    Pin.as_activity({user: 1, item: 4});
-    Pin.as_activity({user: 2, item: 6});
-    Pin.as_activity({user: 1, item: 5});
-    Pin.as_activity({user: 1, item: 7});
-    Pin.as_activity({user: 3, item: 9});
-    Pin.as_activity({user: 3, item: 8});
-    Pin.as_activity({user: 3, item: 4});
-
-    Follow.as_activity({user: 1, target: 2});
-    Follow.as_activity({user: 1, target: 3});
-    Follow.as_activity({user: 2, target: 1});
-    Follow.as_activity({user: 2, target: 3});
+    async.series([
+        function(cb){
+            User.create({username: 'admin', avatar_url: 'admin.jpg'}, function(){
+                User.create({username: 'Andrew', avatar_url: 'https://github.com/identicons/jasonlong.png'});
+                User.create({username: 'Sergey', avatar_url: 'https://avatars0.githubusercontent.com/u/4436860?v=3&s=96'});
+                User.create({username: 'Thomas', avatar_url: 'https://avatars0.githubusercontent.com/u/125464?v=3&s=96'});
+                cb(null);
+            });
+        },
+        function(cb){
+           var mediaFiles = fs.readdirSync('./media/');
+           mediaFiles.forEach(function(fileName){
+                Item.create({user: 0, image_url: fileName}, function(err, newArticle){
+                    if (err){
+                        return console.log(err);
+                    }
+                });
+            });
+            cb(null);
+        },
+        function(cb){
+            Pin.as_activity({user: 1, item: 1});
+            Pin.as_activity({user: 1, item: 2});
+            Pin.as_activity({user: 1, item: 3});
+            Pin.as_activity({user: 1, item: 4});
+            Pin.as_activity({user: 2, item: 6});
+            Pin.as_activity({user: 1, item: 5});
+            Pin.as_activity({user: 1, item: 7});
+            Pin.as_activity({user: 3, item: 9});
+            Pin.as_activity({user: 3, item: 8});
+            Pin.as_activity({user: 3, item: 4});
+            cb(null);
+        },
+        function(cb){
+            Follow.as_activity({user: 1, target: 2});
+            Follow.as_activity({user: 1, target: 3});
+            Follow.as_activity({user: 2, target: 1});
+            Follow.as_activity({user: 2, target: 3});
+        }
+    ]);
 };
