07d39f10a1c86dbf0fe34f39976fbd18b0a0f101
mihaineacsu <mihai.neacsu@ymail.com>
2015-03-09 14:50:24 +0200

    Github middleware won't accept empty string, use dummy text

--- config/config.js
+++ config/config.js
@@ -5,11 +5,11 @@ nconf.argv().env();
 var STREAM_URL = process.env.STREAM_URL.replace(/[@=:]/g, '/').split('/');
 
 nconf.defaults({
-    'MONGOLAB_URI': 'mongodb://localhost/streamapp',
+    'MONGOLAB_URI': 'mongodb://localhost/stream_nodejs',
 	'PORT': 8000,
-	'GITHUB_CLIENT_ID': '',
-	'GITHUB_CLIENT_SECRET': '',
-	'GITHUB_CALLBACK': '',
+	'GITHUB_CLIENT_ID': 'REPLACE_W_GITHUB_CLIENT_ID',
+	'GITHUB_CLIENT_SECRET': 'REPLACE_W_GITHUB_CLIENT_SECRET',
+	'GITHUB_CALLBACK': '/auth/github/callback',
 	'STREAM_ID': STREAM_URL[7],
 	'STREAM_API_KEY': STREAM_URL[3],
 	'STREAM_API_SECRET': STREAM_URL[4]
