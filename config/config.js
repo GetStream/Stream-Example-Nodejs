59fd413fda6d8618fe8e33c7c68945f4796ddac8
Tommaso Barbugli <tbarbugli@gmail.com>
2015-04-21 17:18:00 +0200

    remove old configs

--- config/config.js
+++ config/config.js
@@ -8,9 +8,6 @@ nconf.defaults({
 	'GITHUB_CLIENT_ID': 'REPLACE_W_GITHUB_CLIENT_ID',
 	'GITHUB_CLIENT_SECRET': 'REPLACE_W_GITHUB_CLIENT_SECRET',
 	'GITHUB_CALLBACK': '/auth/github/callback',
-	'STREAM_ID': 2620,
-	'STREAM_API_KEY': 'pp5nmuzmw2n5',
-	'STREAM_API_SECRET': 'gdps5zv24a6xbz7spst5j8ur68639nj2kyg28fwmec3wjuayd38kdzz926a5e3fm'
 });
 
 module.exports = nconf;
