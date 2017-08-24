549d6c37f421d76688c1e38d6a8fc597ea8ad177
Nick Parsons <nparsons08@gmail.com>
2017-08-24 15:33:38 -0600

    Defaults

--- config/config.js
+++ config/config.js
@@ -3,11 +3,11 @@ var nconf = require('nconf');
 nconf.argv().env();
 
 nconf.defaults({
-  'MONGOLAB_URI': 'mongodb://localhost/stream_nodejs',
-	'PORT': 8000,
-	'GITHUB_CLIENT_ID': 'REPLACE_W_GITHUB_CLIENT_ID',
-	'GITHUB_CLIENT_SECRET': 'REPLACE_W_GITHUB_CLIENT_SECRET',
-	'GITHUB_CALLBACK': '/auth/github/callback',
+	MONGOLAB_URI: 'mongodb://localhost/stream_nodejs',
+	PORT: 8000,
+	GITHUB_CLIENT_ID: 'REPLACE_W_GITHUB_CLIENT_ID',
+	GITHUB_CLIENT_SECRET: 'REPLACE_W_GITHUB_CLIENT_SECRET',
+	GITHUB_CALLBACK: '/auth/github/callback',
 });
 
 module.exports = nconf;
